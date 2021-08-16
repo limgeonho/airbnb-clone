0f86971e17c53176d5bfa0fdd4cc4a9d226ab5e0
limgeonho <ghlim909@gmail.com>
2021-08-16 18:18:55 +0900

    2021.08.16

--- rooms/models.py
+++ rooms/models.py
@@ -3,8 +3,6 @@ from django.urls import reverse
 from django_countries.fields import CountryField
 from core import models as core_models
 
-# from users import models as user_models
-
 
 class AbstractItem(core_models.TimeStampedModel):
 
@@ -39,6 +37,8 @@ class Facility(AbstractItem):
 
     """Facility Model Definition"""
 
+    pass
+
     class Meta:
         verbose_name_plural = "Facilities"
 
@@ -68,57 +68,34 @@ class Room(core_models.TimeStampedModel):
     """Room Model Definition"""
 
     name = models.CharField(max_length=140)
-
     description = models.TextField()
-
     country = CountryField()
-
     city = models.CharField(max_length=80)
-
     price = models.IntegerField()
-
     address = models.CharField(max_length=140)
-
-    guests = models.IntegerField()
-
+    guests = models.IntegerField(help_text="How many people will be staying?")
     beds = models.IntegerField()
-
     bedrooms = models.IntegerField()
-
     baths = models.IntegerField()
-
     check_in = models.TimeField()
-
     check_out = models.TimeField()
-
     instant_book = models.BooleanField(default=False)
-
     host = models.ForeignKey(
         "users.User", related_name="rooms", on_delete=models.CASCADE
     )
-    # User에 있는 내용을 import해서 사용하기 위함... FK를 이용해서 User와 Room을 연결함.
-    # room은 many이지만 host는 one
-    # related_name = 은 foreign key의 대상이 당신의 element에 접근할 수 있도록 해주는 것이다.
-
     room_type = models.ForeignKey(
-        "RoomType",
-        related_name="rooms",
-        on_delete=models.SET_NULL,
-        null=True,
+        "RoomType", related_name="rooms", on_delete=models.SET_NULL, null=True
     )
-
     amenities = models.ManyToManyField("Amenity", related_name="rooms", blank=True)
-
     facilities = models.ManyToManyField("Facility", related_name="rooms", blank=True)
-
     house_rules = models.ManyToManyField("HouseRule", related_name="rooms", blank=True)
+
+    def __str__(self):
+        return self.name
 
     def save(self, *args, **kwargs):
         self.city = str.capitalize(self.city)
         super().save(*args, **kwargs)
-
-    def __str__(self):
-        return self.name
 
     def get_absolute_url(self):
         return reverse("rooms:detail", kwargs={"pk": self.pk})
