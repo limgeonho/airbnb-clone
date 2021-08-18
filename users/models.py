bd494f1e6367cd390e7087ed2892c9b7004ac187
limgeonho <ghlim909@gmail.com>
2021-08-18 23:16:47 +0900

    2021.08.18

--- users/models.py
+++ users/models.py
@@ -19,35 +19,21 @@ class User(AbstractUser):
     LANGUAGE_ENGLISH = "en"
     LANGUAGE_KOREAN = "kr"
 
-    LANGUAGE_CHOICES = (
-        (LANGUAGE_ENGLISH, "English"),
-        (LANGUAGE_KOREAN, "Korean"),
-    )
+    LANGUAGE_CHOICES = ((LANGUAGE_ENGLISH, "English"), (LANGUAGE_KOREAN, "Korean"))
 
     CURRENCY_USD = "usd"
     CURRENCY_KRW = "krw"
 
-    CURRENCY_CHOICES = (
-        (CURRENCY_USD, "USD"),
-        (CURRENCY_KRW, "KRW"),
-    )
+    CURRENCY_CHOICES = ((CURRENCY_USD, "USD"), (CURRENCY_KRW, "KRW"))
 
-    avatar = models.ImageField(upload_to="avatars", blank=True, null=True)
-
-    gender = models.CharField(
-        choices=GENDER_CHOICES, max_length=10, blank=True, null=True
-    )
-
+    avatar = models.ImageField(upload_to="avatars", blank=True)
+    gender = models.CharField(choices=GENDER_CHOICES, max_length=10, blank=True)
     bio = models.TextField(blank=True)
-
     birthdate = models.DateField(blank=True, null=True)
-
     language = models.CharField(
-        choices=LANGUAGE_CHOICES, max_length=2, blank=True, null=True
+        choices=LANGUAGE_CHOICES, max_length=2, blank=True, default=LANGUAGE_KOREAN
     )
-
     currency = models.CharField(
-        choices=CURRENCY_CHOICES, max_length=3, blank=True, null=True
+        choices=CURRENCY_CHOICES, max_length=3, blank=True, default=CURRENCY_KRW
     )
-
     superhost = models.BooleanField(default=False)
