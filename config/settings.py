1845ebeb9921585c91dc8a457d19b2f4c9743fda
limgeonho <ghlim909@gmail.com>
2021-08-19 16:47:17 +0900

    2021.08.19

--- config/settings.py
+++ config/settings.py
@@ -140,3 +140,13 @@ AUTH_USER_MODEL = "users.User"
 MEDIA_ROOT = os.path.join(BASE_DIR, "uploads")
 
 MEDIA_URL = "/media/"
+
+# Email Configuration
+
+EMAIL_HOST = "smtp.mailgun.org"
+EMAIL_PORT = "587"
+
+EMAIL_HOST_USER = os.environ.get("MAILGUN_USERNAME")
+EMAIL_HOST_PASSWORD = os.environ.get("MAILGUN_PASSWORD")
+
+EMAIL_FROM = "ghlim909y@sandbox2ba559537f904296851b8b1b0c8d7d24.mailgun.org"
